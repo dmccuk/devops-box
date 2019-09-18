a502a96485fc6cd37819cec6171573b527fa6f2b
Dennis McCarthy <dmccuk@users.noreply.github.com>
2019-09-18 21:05:11 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,29 +1,17 @@
 #!/bin/bash
 set -x
 
-if [ -e /etc/redhat-release ] ; then
-  REDHAT_BASED=true
-fi
-
 TERRAFORM_VERSION="0.11.7"
-PACKER_VERSION="1.2.4"
 # create new ssh key
 [[ ! -f /home/ubuntu/.ssh/mykey ]] \
 && mkdir -p /home/ubuntu/.ssh \
 && ssh-keygen -f /home/ubuntu/.ssh/mykey -N '' \
 && chown -R ubuntu:ubuntu /home/ubuntu/.ssh
 
-# install packages
-if [ ${REDHAT_BASED} ] ; then
-  yum -y update
-  yum install -y docker ansible unzip wget
-else 
-  apt-add-repository ppa:ansible/ansible
-  apt-get update
-  apt-get -y install docker.io ansible unzip
-fi
-# add docker privileges
-usermod -G docker ubuntu
+apt-add-repository ppa:ansible/ansible
+apt-get update
+apt-get -y install ansible unzip
+
 # install pip
 pip install -U pip && pip3 install -U pip
 if [[ $? == 127 ]]; then
@@ -35,25 +23,8 @@ fi
 pip install -U awscli
 pip install -U awsebcli
 
-#terraform
-T_VERSION=$(/usr/local/bin/terraform -v | head -1 | cut -d ' ' -f 2 | tail -c +2)
-T_RETVAL=${PIPESTATUS[0]}
-
-[[ $T_VERSION != $TERRAFORM_VERSION ]] || [[ $T_RETVAL != 0 ]] \
-&& wget -q https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
+wget -q https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip \
 && unzip -o terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin \
 && rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 
-# packer
-P_VERSION=$(/usr/local/bin/packer -v)
-P_RETVAL=$?
-
-[[ $P_VERSION != $PACKER_VERSION ]] || [[ $P_RETVAL != 1 ]] \
-&& wget -q https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip \
-&& unzip -o packer_${PACKER_VERSION}_linux_amd64.zip -d /usr/local/bin \
-&& rm packer_${PACKER_VERSION}_linux_amd64.zip
-
-# clean up
-if [ ! ${REDHAT_BASED} ] ; then
-  apt-get clean
-fi
+apt-get clean
